d9d047c12fb071c4bfec9c43a96f6ba36cca9cb9
Michalis Pardalos <me@mpardalos.com>
2026-01-15 22:00:50 +0000

    Implement DPLL without backtracking

--- exe/Main.hs
+++ exe/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists #-}
 module Main where
 
 import Test.Hspec
@@ -10,14 +11,21 @@ import Data.Function ((&))
 import Data.List (isPrefixOf, isSuffixOf)
 import System.Directory (listDirectory)
 import System.FilePath ((</>), takeFileName)
-import Safe (readNote)
+import Data.Int (Int8)
+import Safe (readNote, headNote)
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (mapMaybe)
+import Debug.Trace (trace)
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 type Atom = Int
 newtype Literal = Literal Atom
-  deriving Show
+  deriving (Show, Eq, Ord)
 type Clause = [Literal]
 type Formula = [Clause]
 
@@ -33,7 +41,10 @@ pattern Negative n <- Literal (\x -> if x < 0 then Just (fromIntegral (negate x)
 
 {-# COMPLETE Positive, Negative #-}
 
-type Valuation = Atom -> Maybe Bool
+negateLit :: Literal -> Literal
+negateLit (Literal n) = Literal (- n)
+
+type Valuation = Set Literal
 
 data Answer = SAT Valuation | UNSAT | UNKNOWN
 
@@ -43,8 +54,10 @@ instance Show Answer where
   show UNKNOWN = "UNKNOWN"
 
 evalLiteral :: Valuation -> Literal -> Bool
-evalLiteral valuation (Positive n) = fromJust $ valuation n
-evalLiteral valuation (Negative n) = not . fromJust $ valuation n
+evalLiteral valuation lit 
+  | Set.member lit valuation = True
+  | Set.member (negateLit lit) valuation = False
+  | otherwise = error ("Not in valuation: " ++ show lit)
 
 evalClause :: Valuation -> Clause -> Bool
 evalClause valuation = any (evalLiteral valuation)
@@ -66,7 +79,7 @@ readCNF txt =
   readClauseLine :: Text -> Clause
   readClauseLine = readClause . map (readNote "literal" . T.unpack) . T.words
 
-  readClause :: [Int] -> Clause
+  readClause :: [Int8] -> Clause
   readClause [] = error "Empty clause"
   readClause [0] = []
   readClause (0:_) = error "Clause does not terminate after 0"
@@ -74,17 +87,44 @@ readCNF txt =
     | x > 0 = Positive x : readClause xs
     | otherwise = Negative (-x) : readClause xs
 
+simplify :: Literal -> Formula -> Formula
+simplify simpLit = mapMaybe simplifyClause
+  where
+    simplifyClause :: Clause -> Maybe Clause
+    simplifyClause [] = Just []
+    simplifyClause (lit:lits)
+      | lit == simpLit =
+        Nothing -- This clause is solved, delete it 
+      | lit == negateLit simpLit =
+        simplifyClause lits -- This literal is impossible, drop it and try the rest
+      | otherwise =
+        (lit :) <$> simplifyClause lits -- Not this lit, continue
+
+-- | Is this formula true for all assignments
+isTriviallyValid :: Formula -> Bool
+isTriviallyValid = null -- If no clauses, then trivially true
+
+-- | Is this formula false for all assignments
+isTriviallyUnsat :: Formula -> Bool
+isTriviallyUnsat = any null -- The empty clause is unsat
+
+chooseLit :: Formula -> Literal
+chooseLit =
+  headNote "Cannot choose lit from formula with empty clause"
+  . headNote "Cannot choose lit from empty formula"
+
 checkSat :: Formula -> Answer
-checkSat [] = UNKNOWN
-checkSat (clause:clauses) =
-  let !_ = checkClause clause
-  in checkSat clauses
- where
-   checkClause [] = UNKNOWN
-   checkClause (lit:lits) = case lit of
-     Positive _ -> checkClause lits
-     Negative _ -> checkClause lits
-    
+checkSat = checkSatWith Set.empty
+  where
+    checkSatWith :: Valuation -> Formula -> Answer
+    checkSatWith valuation formula
+      | isTriviallyUnsat formula =
+        trace ("Best I managed was: " ++ show valuation) UNSAT
+      | isTriviallyValid formula = SAT valuation
+      | otherwise =
+        let lit = chooseLit formula
+        in checkSatWith (Set.insert lit valuation) (simplify lit formula)
+
 testDir :: FilePath
 testDir = "tests"
 
