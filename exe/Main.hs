a27f427bdf58ab984cc4a88ba09468d2aa0dd244
Michalis Pardalos <me@mpardalos.com>
2026-01-18 17:58:17 +0000

    Keep track of decision levels

--- exe/Main.hs
+++ exe/Main.hs
@@ -3,18 +3,19 @@ module Main where
 import Control.Monad (when)
 import Data.Foldable (forM_)
 import Data.IntMap qualified as IntMap
-import Data.List (intercalate)
+import Data.List (intercalate, sortBy)
 import Data.Text.IO qualified as TIO
 import Data.Vector qualified as V
 import Lilsat
 import System.Environment (getArgs)
 import Text.Printf (printf)
+import Data.Ord (comparing)
 
 printValuation :: Formula -> Valuation -> IO ()
 printValuation formula valuation =
   mapM_
     (uncurry (printValue 0))
-    (IntMap.toList valuation)
+    (sortBy (comparing (reason . snd)) $ IntMap.toList valuation)
   where
     printIndent :: Int -> IO ()
     printIndent level = do
@@ -22,24 +23,24 @@ printValuation formula valuation =
       when (level > 0) $ putStr "⤷ "
 
     printValue :: Int -> Int -> VariableData -> IO ()
-    printValue level lit VariableData {value, reason}
-      | level > 4 = do
-          printIndent level
+    printValue nest lit VariableData {value, reason}
+      | nest > 2 = do
+          printIndent nest
           putStrLn "..."
       | otherwise = do
-          printIndent level
+          printIndent nest
           let atom = abs lit
               negation = if value then "" else "¬"
           printf "%s%d" negation atom
           case reason of
-            Decision -> printf " by decision\n"
-            Consequence n -> do
-              let clause = formula V.! n
-              printf " by #%d (%s)\n" n (showClause clause)
+            Decision {level} -> printf " by decision %d\n" level
+            Implied {antecedent, level} -> do
+              let clause = formula V.! antecedent
+              printf " by #%d (%s) (at level %d)\n" antecedent (showClause clause) level
               forM_ clause $ \(Literal causeLit) ->
-                when (causeLit /= lit) $
+                when (abs causeLit /= abs lit) $
                   let valuationLit = if IntMap.member causeLit valuation then causeLit else (-causeLit)
-                   in printValue (level + 1) valuationLit (valuation IntMap.! valuationLit)
+                   in printValue (nest + 1) valuationLit (valuation IntMap.! valuationLit)
 
     showClause :: Clause -> String
     showClause =
